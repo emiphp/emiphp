d1c6a9ab875dbd9afb3089bc7353865a775be26a
emi_nem92_ <emi_nem92_@abv.bg>
2015-05-22 20:33:37 +0300

    domashno

---
+++ 06.for/for.php
@@ -0,0 +1,43 @@
+<!doctype html> 
+ <head> 
+
+
+
+     <title>CSS Selectors</title> 
+     <style> 
+ 	 
+		/* Tag Selector - selects all the divs on the page */ 
+        div{ 
+ 			background-color:blue; 
+ 	   	} 
+ 	    
+ 		/* ID Selector - selects the element in the page with id first-span */ 
+        #first-span { 
+        	else
+             background-color:green;         } 
+ 		 
+       
+         } 
+ 		 
+ 		/* Tag Selector - selects the body of the page */ 
+ 		body { 
+ 			font-size:32px; 
+ 		}
+ 	</style>
+
+ </head>
+ <body>
+dfsdfd
+<?php
+for( $i = 0; $i < 100; $i++ ){
+
+	
+}
+?>
+
+ </body> 
+ </html> 
+ 
+
+
+ 
