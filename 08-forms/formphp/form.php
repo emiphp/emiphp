9192f9719b491f67ba29e9aa0e8bad12ce7ede1c
emi_nem92_ <emi_nem92_@abv.bg>
2015-05-30 10:57:38 +0300

    form

---
+++ 08-forms/formphp/form.php
@@ -0,0 +1,15 @@
+<html>
+
+<body>
+
+	<form>
+		<div>
+			<span>ime:</span>
+			<input type="text" name="name"/>
+		</div>
+		<div> 
+			<input type="submit"value="izprati"/>
+		</div>
+	</form>
+</body>
+</html>
